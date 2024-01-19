6bb4867167eda46b3a1e7dce1112489afebb69d1
coo0 <coo089@live.com>
2024-01-19 11:17:05 +0800

    添加ip缓存 减轻发送压力

--- api/ether_info.go
+++ api/ether_info.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 )
@@ -52,48 +53,72 @@ func (i *IKuai) ShowEtherInfoByComment(url []string, hostname [][]string, token 
 		}
 	}
 	log.Println("外网ip：" + IpAddr)
-	for i, u := range url {
-		token := token[i]
-		for _, hname := range hostname[i] {
-			urlStr := ""
-			//判断字符串中是否包含字符
-			if strings.Contains(u, "dynv6.com") {
-				urlStr = u + "?hostname=" + hname + "&token=" + token + "&ipv4=" + IpAddr
-			} else if strings.Contains(u, "dedyn.io") {
-				urlStr = u + "?hostname=" + hname + "&myipv4=" + IpAddr
-			}
-			if urlStr == "" {
-				continue
-			}
-			fmt.Println(urlStr)
-			client := &http.Client{}
-			req, err := http.NewRequest("GET", urlStr, nil)
-			if err != nil {
-				fmt.Println(1)
-				log.Fatal(err)
-			}
-			if strings.Contains(u, "dedyn.io") {
-				req.Header.Set("Authorization", "Token "+token)
-			}
-
-			resp, err := client.Do(req)
-			if err != nil {
-				fmt.Println(2)
-				log.Fatal(err)
-			}
-			defer resp.Body.Close()
-			bodyText, err := io.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println(4)
-				log.Println(err)
-			} else {
-				log.Println(hname + "绑定到" + IpAddr + ":" + string(bodyText))
-			}
-			time.Sleep(time.Second * 3)
-
+	filePath := "./ip_tmp/" + IpAddr
+	exists, err := fileExists(filePath)
+	if err != nil {
+		log.Printf("检查过程中发生错误: %v", err)
+	} else if exists {
+		log.Println("文件存在")
+	} else {
+		file, err := os.Create(filePath)
+		if err != nil {
+			log.Println("创建文件时发生错误:", err)
+			return err
 		}
-		time.Sleep(time.Second * 1)
-	}
+		defer file.Close()
+		for i, u := range url {
+			token := token[i]
+			for _, hname := range hostname[i] {
+				urlStr := ""
+				//判断字符串中是否包含字符
+				if strings.Contains(u, "dynv6.com") {
+					urlStr = u + "?hostname=" + hname + "&token=" + token + "&ipv4=" + IpAddr
+				} else if strings.Contains(u, "dedyn.io") {
+					urlStr = u + "?hostname=" + hname + "&myipv4=" + IpAddr
+				}
+				if urlStr == "" {
+					continue
+				}
+				fmt.Println(urlStr)
+				client := &http.Client{}
+				req, err := http.NewRequest("GET", urlStr, nil)
+				if err != nil {
+					fmt.Println(1)
+					log.Fatal(err)
+				}
+				if strings.Contains(u, "dedyn.io") {
+					req.Header.Set("Authorization", "Token "+token)
+				}
 
+				resp, err := client.Do(req)
+				if err != nil {
+					fmt.Println(2)
+					log.Fatal(err)
+				}
+				defer resp.Body.Close()
+				bodyText, err := io.ReadAll(resp.Body)
+				if err != nil {
+					fmt.Println(4)
+					log.Println(err)
+				} else {
+					log.Println(hname + "绑定到" + IpAddr + ":" + string(bodyText))
+				}
+				time.Sleep(time.Second * 3)
+
+			}
+			time.Sleep(time.Second * 1)
+		}
+	}
 	return nil
+}
+
+func fileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil // 文件存在且无错误
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil // 文件不存在
+	}
+	return false, err // 其他错误（比如权限问题）
 }
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 )
 
@@ -22,6 +23,7 @@ var conf struct {
 	Hostname  [][]string `yaml:"hostname"`
 	Token     []string   `yaml:"Token"`
 	Cron      string     `yaml:"cron"`
+	CronDel   string     `yaml:"crondel"`
 }
 
 func main() {
@@ -32,6 +34,13 @@ func main() {
 		log.Println("读取配置文件失败：", err)
 		return
 	}
+	//判断目录是或否存在，不存在就创建
+	err = ensureDirectoryExist()
+	if err != nil {
+		log.Println("生成ip文件夹失败：", err)
+		return
+	}
+	//deleteFilesInDirectory()
 
 	update() //绑定ip到dynv6
 
@@ -40,6 +49,13 @@ func main() {
 	}
 
 	c := cron.New()
+	_, err = c.AddFunc(conf.CronDel, deleteFilesInDirectory)
+	if err != nil {
+		log.Println("启动定时清空文件夹计划任务失败：", err)
+		return
+	} else {
+		log.Println("已启动定时清空文件夹计划任务")
+	}
 	_, err = c.AddFunc(conf.Cron, update)
 	if err != nil {
 		log.Println("启动计划任务失败：", err)
@@ -90,9 +106,56 @@ func update() {
 	err = iKuai.ShowEtherInfoByComment(conf.UpdateApi, conf.Hostname, conf.Token)
 
 	if err != nil {
-		log.Println("发送失败：", err)
+		log.Println("运行失败：", err)
 		return
 	} else {
-		log.Println("发送成功")
+		log.Println("运行成功")
 	}
+}
+
+func ensureDirectoryExist() error {
+	// 检查目录是否存在
+	path := "./ip_tmp"
+	_, err := os.Stat(path)
+	if err == nil {
+		// 目录已存在，无需操作
+		return nil
+	}
+	if os.IsNotExist(err) {
+		// 目录不存在，尝试创建
+		err = os.MkdirAll(path, 0755)
+		if err != nil {
+			log.Printf("创建目录失败: %s\n", err)
+		}
+		return nil
+	}
+	// 其他错误情况，直接返回错误信息
+	return err
+}
+
+// 删除文件夹下的所有文件
+func deleteFilesInDirectory() {
+	// 打开目录
+	dirPath := "./ip_tmp"
+	d, err := os.Open(dirPath)
+	if err != nil {
+		log.Printf("打开目录失败: %s\n", err)
+	}
+	defer d.Close()
+
+	// 遍历目录中的所有元素
+	files, err := d.Readdir(-1)
+	if err != nil {
+		log.Printf("读取目录内容失败: %s", err)
+	}
+
+	// 遍历并删除每个元素
+	for _, fileInfo := range files {
+		fullPath := filepath.Join(dirPath, fileInfo.Name())
+		err = os.Remove(fullPath)
+		if err != nil {
+			log.Printf("删除文件失败: %s", err)
+		}
+	}
+
 }
